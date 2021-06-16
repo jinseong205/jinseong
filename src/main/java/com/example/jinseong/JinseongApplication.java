812df1a8f91861a3aac5f0b8d14dd88a02e741ca
jinseong <jinseong@DESKTOP-RFE7VCD>
2021-06-16 10:41:34 +0900

    1

---
+++ src/main/java/com/example/jinseong/JinseongApplication.java
@@ -0,0 +1,13 @@
+package com.example.jinseong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinseongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinseongApplication.class, args);
+	}
+
+}
