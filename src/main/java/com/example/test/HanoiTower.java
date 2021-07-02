1f935d57d406dd29379fa0f546cac43c78feb5c8
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-07-02 15:56:02 +0900

    hanoi

---
+++ src/main/java/com/example/test/HanoiTower.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+public class HanoiTower {
+
+	public static void main(String[] args) {
+		HanoiTower h = new HanoiTower();
+		h.solution(0);
+	}
+	public void solution(int n) {
+		
+		hanoi(3, 1,2,3);
+	}
+	
+	void hanoi(int n, int start, int mid, int to ) {
+		if(n == 1 ) {
+			System.out.println(start + " -> " + to);
+		    return;
+		}
+		
+		hanoi(n - 1, start, to, mid);
+		System.out.println(start + " -> " + to );
+		hanoi(n - 1, mid, start, to);
+	}
+}
