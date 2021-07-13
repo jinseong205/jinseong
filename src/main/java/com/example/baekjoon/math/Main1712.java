afb5fd2eb87898f2e23cc8f0c94a83d0ad0162e2
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-07-13 16:51:25 +0900

    1712

---
+++ src/main/java/com/example/baekjoon/math/Main1712.java
@@ -0,0 +1,27 @@
+/**
+ * 수학1 - 1712 - 손익분기점
+ */
+package com.example.baekjoon.math;
+
+import java.util.Scanner;
+
+public class Main1712 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if( c <= b ) {
+			System.out.println(-1);
+			return;
+		}
+		
+		int answer = a / (c-b) + 1;
+		System.out.println(answer);
+		
+	}
+
+
+}
