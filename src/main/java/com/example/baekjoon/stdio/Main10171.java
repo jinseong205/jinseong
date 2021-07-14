57433cc9a64ec7b99df22265ea3b4ca558248738
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-07-14 16:03:37 +0900

    10171

---
+++ src/main/java/com/example/baekjoon/stdio/Main10171.java
@@ -0,0 +1,18 @@
+/**
+ * 표준 입출력과 사칙 연산 - 10171 - 고양이
+ */
+package com.example.baekjoon.stdio;
+
+
+public class Main10171 {
+
+	public static void main(String[] args)  {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|"); 
+	}
+
+}
+	
