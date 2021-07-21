a0fb3cf37e794bcf826432a178460c6c467c2c70
jinseong205 <jinseong205@gmail.com>
2021-07-21 21:58:49 +0900

    9498

---
+++ src/main/java/com/example/baekjoon/ifEx/Main9498.java
@@ -0,0 +1,32 @@
+/**
+ * if문 - 9498 - 시험성적
+ */
+package com.example.baekjoon.ifEx;
+
+import java.io.*;
+
+public class Main9498 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int score = Integer.parseInt(str);
+		
+			
+		if(score >= 90 && score <= 100) {
+			System.out.println("A");
+		}else if(score >= 80 && score < 90) {
+			System.out.println("B");
+		}else if(score >= 70 &&  score < 80) {
+			System.out.println("C");
+		}else if(score >= 60 && score < 70) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+		
+	}
+
+}
+	
